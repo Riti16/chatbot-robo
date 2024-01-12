12919945b1bed688b650a4848221c9846857b700
Riti Dass <37213335+Riti16@users.noreply.github.com>
2024-01-12 15:54:59 +0530

    Update chatbot_robo.py

--- chatbot_robo.py
+++ chatbot_robo.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-
-
-# chatbot-robo
-It is a very simple bot with hardly any cognitive skills. Though 'ROBO' responds to user input, it won't fool your friends. This example will help us to think through the design and challenge of creating a chatbot.
 
 # coding: utf-8
 
